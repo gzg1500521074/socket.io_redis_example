9e3cfcf16b5a1041b8026294f5ccfc02484b63bb
itahirov <38325195+itahirov@users.noreply.github.com>
2021-02-02 21:08:02 -0500

    Update config.js
    removed secret

--- Socket/config.js
+++ Socket/config.js
@@ -4,9 +4,9 @@ config.redis = {};
 config.server = {};
 
 // Redis Configuration
-config.redis.uri = process.env.REDIS_URI || 'istahirediscache.redis.cache.windows.net';
+config.redis.uri = process.env.REDIS_URI || 'istahirediscache.redis.cache.windows.net';  //need to use your own redis cache
 config.redis.port = process.env.REDIS_PORT || 6379;
-config.redis.key = process.env.REDIS_KEY || 'GaJB++WM4fEe6MWdP+DbW5cdcKWUy5pcrkzmaSWlN+c=';
+config.redis.key = process.env.REDIS_KEY || KEY; //need to use authentication key of you azure redis cache (or custom one) 
 config.redis.channel = process.env.REDIS_CHANNEL || 'chat';
 
 // Server Configuration
